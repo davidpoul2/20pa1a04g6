65a2d5bdfed5ec1d4fab9ec886c5df5b270dddc4
davidpoul2 <dvistarla9@gmail.com>
2024-05-23 12:10:45 +0530

    Third Commit

---
+++ dev1.java
@@ -0,0 +1,5 @@
+public class dev1{
+	public static void main(String args[]){
+		System.out.println("Hello this is dev1");
+	}
+}
---
+++ g1.java
@@ -0,0 +1,6 @@
+public class g1{
+	public static void main(String agrs[]){
+		System.out.println("Hell0");
+	}
+}
+//Hello World.
